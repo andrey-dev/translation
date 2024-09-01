53d80c126740256f4061fcd7f7b35fd7951edf92
Andrei Golovin <andrei@email.com>
2024-09-01 17:05:23 +0200

    First commit

---
+++ public/main.js
@@ -0,0 +1,34 @@
+function sendRequest() {
+  const formData = {
+    name: document.getElementById("name").value,
+    phone: document.getElementById("phone").value,
+    email: document.getElementById("email").value,
+    comment: document.getElementById("comment").value || '',
+  };
+
+  if (!(formData.name && (formData.phone || formData.email))) {
+    console.warn('Name and phone or email should be provided');
+    return;
+  }
+  // name, email, phone, comment
+  fetch("http://localhost:3000/request", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((jsonResponse) => {
+      const okElem = document.getElementById("status-ok");
+      const errElem = document.getElementById("status-err");
+      okElem.classList.remove("status-visible");
+      errElem.classList.remove("status-visible");
+
+      if (jsonResponse.success) {
+        okElem.classList.add("status-visible");
+      } else {
+        errElem.classList.add("status-visible");
+      }
+    });
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const { log } = require('console');
+const express = require('express');
+const path = require('path');
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 3000;
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "sid.garbage.mail@gmail.com",
+    pass: "jlif onjc zvey ifdt",
+    // pass: "garbage/*-+01",
+  },
+});
+
+app.use(express.json());
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  // res.send('Welcome to my server!');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.post('/request', (req, res) => {
+  console.log('POST request with ', req.body);
+  (async () => {
+    const result = await sendEmail(req.body);
+    res.json({success : result});
+    res.end();
+  })();
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+async function sendEmail(body) {
+  const mailOptions = {
+    from: '"Sid Freeman" <sid.garbage.mail@gmail.com>',
+    to: "agolovin.dev@gmail.com",
+    subject: `✔ New order from ${body.name} ✔`,
+    text: `
+    name: ${body.name}
+    phone: ${body.phone}
+    email: ${body.email}
+    
+    comment: ${body.comment}
+    `,
+  };
+
+  return new Promise((resolve) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Failed to send an email', error);
+        resolve(false);
+      } else {
+        console.info('Email sent', info);
+        resolve(true);
+      }
+    });
+  });
+}
